1052b7b280640a0f1e74619ac0aa1940e0443ef8
jake_hudson <jacobhudson107@gmail.com>
2023-02-09 17:06:31 +0000

    got gene_phylogeny script to run jackhmmer

--- 20230207_Bacteria/scripts/20230209_gene_phylogeny.sh
+++ 20230207_Bacteria/scripts/20230209_gene_phylogeny.sh
@@ -8,11 +8,12 @@ Help()
 {
 	# Display Help
 	echo "Given a gene, this script will search for related genes with
-	      jackhmmmer, then produce a phylogentic tree of related genes"
+jackhmmmer, then produce a phylogentic tree of related genes"
 	echo 
 	echo "options:"
 	echo "  -f    file to run on"
 	echo "  -h    display this help"
+	echo "  -b    directory to build database from"
 	echo 
 }
 
@@ -21,7 +22,7 @@ Help()
 #############################################
 
 # Get options
-while getopts ":hf:" option;
+while getopts ":hf:b:" option;
 do
 	case $option in
 		h) # display Help
@@ -29,6 +30,8 @@ do
 			exit;;
 		f) # file to be run on
 			file=$OPTARG;;
+		b) #directory to build hmm database from
+		  dir=$OPTARG;;
 		\?) # Invalid option
 			echo "Error: Invalid option"
 			Help
@@ -39,6 +42,21 @@ done
 ##############################################################
 # Main Program
 #############################################################
+
+#find cds files to unzip
+echo "finding and unzipping files in ${dir}"
+find $dir -type f -name *_cds_*.fna.gz -exec gzip -d -f -k {} +
+
+#concatenate them all together so jackhmmer can search something
+echo "finding file in ${dir} to build database from"
+find $dir -type f -name *_cds_*.fna -exec cat {} + > ${dir}/database.fna
+
+#run jackhmmer
+echo "running jackhmmer"
+jackhmmer -o ${dir}/jackhmmer.out -A ${dir}/jackhmmer_msa.fna $file ${dir}/database.fna
+
+
+
 
 
 
